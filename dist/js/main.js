9a80278698c18c4749ba52b23c49dadd1b71185a
DuyLong2001 <long01229077706@gmail.com>
2023-01-03 16:35:16 +0700

    second commit

---
+++ dist/js/main.js
@@ -0,0 +1,24 @@
+// script. 
+// 	$(document).ready(function(){
+// 		$(window).scroll(function(){
+// 			if($(this).scrollTop()){
+// 					$('header').addClass('sticky');
+// 			}
+// 			else{
+// 					$('header').removeClass('sticky');
+// 			}
+// 		});
+// 	});
+// script. 
+// 	window.onscroll = function(){
+// 		scroll();
+// 	};
+
+// 	function scroll(){
+// 		if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+// 			document.getElementById("chat").style.top = "0";
+// 		}
+// 		else{
+// 			document.getElementById("chat").style.top = "-60px";
+// 		}
+// 	}
